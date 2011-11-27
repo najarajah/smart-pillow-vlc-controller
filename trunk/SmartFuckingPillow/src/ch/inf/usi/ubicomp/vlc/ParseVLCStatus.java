7784389ad54ac5feb20508e5d066463bff36dc43
R.Minelli88 <R.Minelli88@18376967-f83f-623b-bed5-551025dd1b98>
2011-11-27 07:51:08 +0000

    Adding all the files we worked yesterday and the week before

---
+++ trunk/SmartFuckingPillow/src/ch/inf/usi/ubicomp/vlc/ParseVLCStatus.java
@@ -0,0 +1,101 @@
+package ch.inf.usi.ubicomp.vlc;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+public class ParseVLCStatus {
+	
+	private DocumentBuilder db;
+	private InputStream is;
+	private Document dom;
+	private Element root;
+	
+	public ParseVLCStatus(){
+		try {
+			is = new ByteArrayInputStream(VLCCommands.getStatus().getBytes("UTF-8"));
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			db = dbf.newDocumentBuilder();
+			dom = db.parse(is);
+			root = dom.getDocumentElement();
+			
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public Element getElement(String elementName){
+		reloadXML();
+		NodeList nl = root.getElementsByTagName(elementName);
+		Element el = (Element)nl.item(0);
+		//System.out.println(el.getFirstChild().getNodeValue());
+		return el;
+		
+	}
+	
+	public int getVolume(){
+		Element el = getElement("volume");
+		return Integer.parseInt(el.getFirstChild().getNodeValue());	
+	}
+	
+	public int getLenght(){
+		Element el = getElement("length");
+		return Integer.parseInt(el.getFirstChild().getNodeValue());	
+	}
+	
+	public int getPosition(){
+		Element el = getElement("position");
+		return Integer.parseInt(el.getFirstChild().getNodeValue());
+	}
+	
+	public void reloadXML(){
+		try {
+			is = new ByteArrayInputStream(VLCCommands.getStatus().getBytes("UTF-8"));
+			dom = db.parse(is);
+			root = dom.getDocumentElement();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+
+	/*public static void main(String[] args){
+		
+		ParseVLCStatus vlcParser = new ParseVLCStatus("<root> <volume>200</volume></root>");
+		System.out.println(vlcParser.getVolume());
+		
+	}*/
+	
+	
+}
+
---
+++ trunk/SmartFuckingPillow/src/ch/inf/usi/ubicomp/vlc/VLCCommands.java
@@ -0,0 +1,126 @@
+package ch.inf.usi.ubicomp.vlc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class VLCCommands
+{
+	/**
+	 * Sends an HTTP GET request to a url
+	 *
+	 * @param endpoint - The URL of the server. (Example: " http://www.yahoo.com/search")
+	 * @param requestParameters - all the request parameters (Example: "param1=val1&param2=val2"). Note: This method will add the question mark (?) to the request - DO NOT add it yourself
+	 * @return - The response from the end point
+	 * 
+	 * 
+	 */
+	
+	public ParseVLCStatus parseVLC;
+	
+	public VLCCommands(){
+		parseVLC = new ParseVLCStatus();
+		
+	}
+	
+	public static String sendGetRequest(String endpoint, String requestParameters){
+		String result = null;
+		if (endpoint.startsWith("http://"))
+		{
+			// Send a GET request to the servlet
+			try
+			{
+
+				// Send data
+				String urlStr = endpoint;
+				if (requestParameters != null && requestParameters.length () > 0)
+				{
+					urlStr += "?" + requestParameters;
+				}
+				URL url = new URL(urlStr);
+				URLConnection conn = url.openConnection ();
+
+				// Get the response
+				BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				StringBuffer sb = new StringBuffer();
+				String line;
+				while ((line = rd.readLine()) != null)
+				{
+					sb.append(line);
+				}
+				rd.close();
+				result = sb.toString();
+				//System.out.println(result);
+			} catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	public String playCommand(){
+		return sendGetRequest("http://localhost:8080/requests/status.xml", "command=pl_play");
+	}
+	
+	public String pauseCommand(){
+		return sendGetRequest("http://localhost:8080/requests/status.xml", "command=pl_pause");
+	}
+	
+	public String stopCommand(){
+		return sendGetRequest("http://localhost:8080/requests/status.xml", "command=pl_stop");
+	}
+	
+	public String nextCommand(){
+		return sendGetRequest("http://localhost:8080/requests/status.xml", "command=pl_next");
+	}
+	
+	public String prevCommand(){
+		return sendGetRequest("http://localhost:8080/requests/status.xml", "command=pl_previous");
+	}
+	
+	public String fullScreenCommand(){
+		return sendGetRequest("http://localhost:8080/requests/status.xml", "command=fullscreen");
+	}
+	
+	public String increaseVolumeCommand(){
+		return sendGetRequest("http://localhost:8080/requests/status.xml", "command=volume&val=%2B20");
+	}
+	
+	public String decreaseVolumeCommand(){
+		return sendGetRequest("http://localhost:8080/requests/status.xml", "command=volume&val=-20");
+	}
+	
+	public static String getStatus(){
+		return sendGetRequest("http://localhost:8080/requests/status.xml", null);
+	}
+	
+	public String increaseSeekCommand(){
+		//check the position
+		int actualPosition = parseVLC.getPosition();
+		int newPosition = actualPosition;
+		if(actualPosition <= 90){
+			newPosition = actualPosition+10;
+			return sendGetRequest("http://localhost:8080/requests/status.xml", "command=seek&val="+newPosition+"%25");
+		}else{
+			return "not send";
+		}
+				
+	}
+	
+	public String decreaseSeekCommand(){
+		//check the position
+		int actualPosition = parseVLC.getPosition();
+		int newPosition = actualPosition;
+		if(actualPosition >=10){
+			newPosition = actualPosition-10;
+			return sendGetRequest("http://localhost:8080/requests/status.xml", "command=seek&val="+newPosition+"%25");
+		}else{
+			return "not send";
+		}			
+			
+	}
+	
+	
+}

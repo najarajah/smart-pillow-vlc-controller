1b587ba4793969c6ba8ac31ff7c334ac7e5a71e0
roberto.guidi.86@gmail.com <roberto.guidi.86@gmail.com@18376967-f83f-623b-bed5-551025dd1b98>
2011-12-06 22:52:14 +0000

    Added reading of 4 flex and 1 pressure in arduino, added functions to detect the kind of event received in java

---
+++ trunk/SmartFuckingPillow/src/ch/inf/usi/ubicomp/pillow/Constants.java
@@ -0,0 +1,9 @@
+package ch.inf.usi.ubicomp.pillow;
+
+public interface Constants {
+	public static  final String FLEX_RED_BENT = "red-bent";
+	public static  final String FLEX_BLUE_BENT = "blue-bent";
+	public static  final String FLEX_GREEN_BENT = "green-bent";
+	public static  final String FLEX_YELLOW_BENT = "yellow-bent";
+	public static final String PRESSURE_TRIGGERED = "pressure-triggered";
+}
